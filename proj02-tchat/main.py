23327b6a0dc966fab6e51e2bc7b587d50c7a4e66
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-11-24 14:51:05 +0700

    finish section 3

--- proj02-tchat/main.py
+++ proj02-tchat/main.py
@@ -1,16 +1,19 @@
+from re import T
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+chat = ChatOpenAI(verbose=True)
+
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
     memory_key="messages",
     return_messages=True,
+    llm=chat,
 )
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
@@ -23,7 +26,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat,
     prompt=prompt,
-    memory=memory
+    memory=memory,
+    verbose=True,
 )
 
 while True:
